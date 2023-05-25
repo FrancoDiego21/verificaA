efa2f58e4ab6a62a3caac4acfa705e831a8af759
FrancoDiego21 <franco.diego@iisgalvanimi.edu.it>
2023-05-25 12:15:25 +0000

    Lavoro finito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Piatti } from './models/piatti.models';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '01_Angular_empty';
+  URL = "https://my-json-server.typicode.com/paolocarugati/semivuota/db"
+  data: Piatti;
+  oPiatti : Observable<Piatti>;
+  
+
+
+  constructor(public http: HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this.oPiatti = this.http.get<Piatti>(this.URL);
+    this.oPiatti.subscribe( d => {
+      this.data = d;
+    });
+  }
 }
---
+++ src/app/primi/primi.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Primo } from '../models/piatti.models';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-primi',
+  templateUrl: './primi.component.html',
+  styleUrls: ['./primi.component.css']
+})
+export class PrimiComponent {
+  primi : Primo[]
+
+  constructor(private router : Router){
+    this.primi = this.router.getCurrentNavigation().extras.state as Primo[]
+  }
+}
